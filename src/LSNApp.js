c9d5ea265f9ec154bcc02d67af1e2ab7b2fbc66b
Jaron <jaronchretien@yahoo.com>
2024-11-06 10:59:53 -0600

    feat(lsnapp): Adds lsnapp part for main part of application

---
+++ src/LSNApp.js
@@ -0,0 +1,12 @@
+import {Route, Routes, useNavigate} from 'react-router-dom'
+
+
+const LSNApp = () => {
+    return(
+        <div className="h-full">
+            
+        </div>
+    )
+}
+
+export default LSNApp
