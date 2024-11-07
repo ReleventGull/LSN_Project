c0c0b8a635abfc44e31b212ee96b033776b72897
Jaron <jaronchretien@yahoo.com>
2024-11-06 22:54:44 -0600

    feat(icons):Adds icons to files

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,11 @@
 
+const images = require.context('../../public/images', false, /\.(png|jpe?g|svg)$/);
 
+const imageList = images.keys().map((path) => ({
+  path: path,
+  src: images(path),
+}));
+    console.log(imageList)
 const Navbar = () => {
     return (
         <div className="h-full p-2 w-20 bg-playbar">
