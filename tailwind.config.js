381cf0d92ad43bf98c5761ffc7e8b46a41c2fdbf
Jaron <jaronchretien@yahoo.com>
2024-11-07 17:47:41 -0600

    feat(svg): Edits svgs adding several shades of gray for navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,9 @@ module.exports = {
         darkPrimary: '#476C9B',
         darkSecondary: '468C98',
         backgroundSecondary: '#1A1B24',
-        backgroundThird: '#2E2F3A'
+        backgroundThird: '#2E2F3A',
+        backgroundFourth: '#6C6D7A',
+        backgroundFifth: '#B1B3C0'
       }
     },
   },
