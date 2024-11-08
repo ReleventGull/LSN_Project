95e95d05394c62d05a2a51cfae8bb3eb4edccd74
Jaron <jaronchretien@yahoo.com>
2024-11-07 18:38:57 -0600

    feat(color): Change color lol

--- src/Components/SettingsCard.js
+++ src/Components/SettingsCard.js
@@ -5,7 +5,7 @@ const SettingsCard = ({card}) => {
     
     return (
         <div onMouseLeave={() => setIsHovered(false)} onMouseEnter={() => setIsHovered(true)} className="group cursor-pointer duration-75 h-12 p-1 w-12 rounded-md bg-backgroundThird hover:bg-backgroundSecondary hover:duration-75">
-        <svg  fill={isHovered ? '#B1B3C0' : card.fillColor} className='h-full duration-75'  viewBox="0 0 24 24"  >
+        <svg  fill={isHovered ? '#FFFFFF' : card.fillColor} className='h-full duration-75'  viewBox="0 0 24 24"  >
             {
                 card.paths.map((item, index) =>
                     <path className='duration-50'  key={index} d={item.d} />
