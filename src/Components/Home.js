56e1785bb165071d27084e901f784734950a1dc5
Jaron <jaronchretien@yahoo.com>
2024-11-11 14:37:50 -0600

    fix(recentCard): Changed font on recent card to match other font sizes for consistency

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -31,7 +31,7 @@ const Home = () => {
 
             <div className="w-full p-2">
                 <h1 className="font-bold mb-1 text-textPrimary">Top Artists</h1>
-                <div className="grid flex gap-2 lg:flex">
+                <div className="grid grid-cols-2 gap-2 lg:flex">
                     {topArtists.map(card =>
                         <TopArtistsCard card={card}/>
                     )}
--- src/Components/TopArtistsCard.js
+++ src/Components/TopArtistsCard.js
@@ -6,11 +6,11 @@ const TopArtistsCard = ({card}) => {
     }
     console.log(card.images[0].height)
     return (
-        <div className="flex flex-col flex-1 w-auto cursor-pointer p-3 rounded-md hover:bg-backgroundSecondary">
+        <div className="flex flex-col flex-1 w-auto cursor-pointer p-2 rounded-md hover:bg-backgroundSecondary">
             <div className="grow rounded-md aspect-w-1 aspect-h-1">
                 <img className="rounded-md" src={card.images[0].url}/>
             </div>
-            <h1 className="m-0 text-textPrimary font-bold">{card.name}</h1>
+            <h1 className="m-0 text-xs text-textPrimary font-bold">{card.name}</h1>
         </div>
     )
 }
