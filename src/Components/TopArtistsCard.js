790873d3cf0fea3f261bff25b5f4a932e8174139
Jaron <jaronchretien@yahoo.com>
2024-11-20 12:43:34 -0600

    feat(recommendations): adds ability to play acrd recommendations

--- src/Components/TopArtistsCard.js
+++ src/Components/TopArtistsCard.js
@@ -1,13 +1,20 @@
+import { playTrack } from "./api"
 
-
-const TopArtistsCard = ({images, card, index}) => {
+const TopArtistsCard = ({images, card, index, deviceId}) => {
     let style = {
         backgroundImage: `url(${images[0].url})`
     }
-
+    const checkCard = async() => {
+        if(card.type == 'artists') {
+            console.log("play artist songs")
+        }else if (card.type == 'track') {
+            console.log("TRACK HERE", card)
+            const response = await playTrack({token: localStorage.getItem("LSNToken"), uri: card.uri, deviceId: deviceId})
+        }
+    }
     return (
         <div className={`recentCard ${index == 6 ? ' last' : index == 5 ? 'secondLast' : index == 4 ? 'thirdLast' : index == 3 ? 'fourthlast' : index == 2 ? 'fifthLast' : ''} group`}>
-            <svg className="recentCardPlayButton group-hover:opacity-100" fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg onClick={checkCard} className="recentCardPlayButton group-hover:opacity-100" fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path className="-translate-y-0.5" d="M21,21H3L12,3Z"/>
             </svg>
             <div className="grow rounded-md aspect-w-1 aspect-h-1">
