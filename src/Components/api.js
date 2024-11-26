858b292b0d731113db40de9ca9cac19a614cb3c3
Jaron <jaronchretien@yahoo.com>
2024-11-26 12:42:31 -0600

    feat(liked): Checks if the current song is on the users liked playlist

--- src/Components/api.js
+++ src/Components/api.js
@@ -145,3 +145,18 @@ export const getAlbumTracks = async({id, token}) => {
         console.error('error fetching artists albums', error)
     }
 }
+
+export const checkIfTrackIsLiked = async ({id, token}) => {
+    try {
+        const response = await fetch(`https://api.spotify.com/v1/me/tracks/contains?ids=${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        const data = await response.json()
+        return data
+    }catch(error) {
+        console.error("Error checking if track is liked", error)
+
+    }
+}
